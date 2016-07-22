be1e10f65ad78a13ce27c735dd59ee9f54dd64e9
pattu <pattu@pattus-MacBook-Pro.local>
2016-07-22 16:52:38 +0530

    First Cut version

---
+++ src/main/java/org/ram/examples/App.java
@@ -0,0 +1,35 @@
+package org.ram.examples;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        ResourceConfig config = new ResourceConfig();
+        config.packages("org.ram.examples");
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+
+        Server server = new Server(9999);
+        ServletContextHandler context = new ServletContextHandler(server, "/*");
+        context.addServlet(servlet, "/*");
+
+
+        try {
+            server.start();
+            server.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            server.destroy();
+        }
+    }
+}
---
+++ src/main/java/org/ram/examples/Resource.java
@@ -0,0 +1,45 @@
+package org.ram.examples;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by pattu on 22/07/16.
+ */
+@Path("calculator")
+public class Resource {
+    @GET
+    @Path("sum")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calculateSum(@QueryParam("input1") String input1, @QueryParam("input2") String input2) {
+        Integer i1 = Integer.parseInt(input1);
+        Integer i2 = Integer.parseInt(input2);
+        return new Integer(i1 + i2).toString();
+    }
+
+    @GET
+    @Path("product")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calculateProduct(@QueryParam("input1") String input1, @QueryParam("input2") String input2) {
+        Integer i1 = Integer.parseInt(input1);
+        Integer i2 = Integer.parseInt(input2);
+        return new Integer(i1 * i2).toString();
+    }
+
+    @POST
+    @Path("post")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_HTML)
+    public String postMethod(@FormParam("input1") String input1, @FormParam("input2") String input2,
+                             @FormParam("operation") String operation) {
+        String result;
+        if (operation.equalsIgnoreCase("sum")) {
+            result = calculateSum(input1, input2);
+        } else if (operation.equalsIgnoreCase("product")) {
+            result = calculateProduct(input2, input2);
+        } else {
+            return "<h2> Unknown operation: " + operation + "</h2>";
+        }
+        return "<h2>Hello, " + input1 + " operation " + input2 + " is " + result + "</h2>";
+    }
+}
